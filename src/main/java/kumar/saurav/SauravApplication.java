8daf06015726a57fff3be49f9e401f36e0da0aee
saurav <sknikukashyap@gmail.com>
2024-02-01 04:01:35 +0530

    initial commit

---
+++ src/main/java/kumar/saurav/SauravApplication.java
@@ -0,0 +1,13 @@
+package kumar.saurav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SauravApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SauravApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kumar/saurav/SauravApplicationTests.java
@@ -0,0 +1,13 @@
+package kumar.saurav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SauravApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
